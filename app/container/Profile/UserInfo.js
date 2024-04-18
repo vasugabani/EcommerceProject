707fe1c841b2a21301fb8f80d07934f791932120
intro <vasugabani1399@gmail.com>
2024-04-18 12:59:25 +0530

    done

--- app/container/Profile/UserInfo.js
+++ app/container/Profile/UserInfo.js
@@ -13,6 +13,7 @@ export default function UserInfo({ navigation }) {
     const dispatch=useDispatch()
 
     const [modal, setmodel] = useState(false)
+    const [isModalVisible, setModalVisible] = useState(false);
 
     const authData = useSelector(state => state.auth)
     console.log(authData, "==============================");
@@ -24,6 +25,14 @@ export default function UserInfo({ navigation }) {
     const handlecross = () => {
         setmodel(false)
     }
+
+    const handleModal = () => {
+        setModalVisible(true)
+    }
+
+    const handleclose = () => {
+        setModalVisible(false);
+    }
 
     const handleGallery = () => {
         ImagePicker.openPicker({
@@ -72,10 +81,11 @@ export default function UserInfo({ navigation }) {
                 <MaterialIcons style={style.icon} name="keyboard-arrow-left" color={'black'} size={34} /></TouchableOpacity>
 
             <View style={{ width: 150, height: 150, backgroundColor: 'black', borderRadius: 100, alignSelf: 'center', marginTop: 20, position: 'relative' }}>
+                <TouchableOpacity onPress={handleModal}>
                 <Image
                     source={{uri:authData.user.imageURL}}
                     style={{ width: '100%', height: '100%', borderRadius: 100 }}
-                />
+                /></TouchableOpacity>
                 <TouchableOpacity onPress={handleToggle}>
                     <MaterialIcons style={style.camera} name="camera-alt" color={'black'} size={34} /></TouchableOpacity>
             </View>
@@ -121,6 +131,22 @@ export default function UserInfo({ navigation }) {
             <TouchableOpacity onPress={handleSubmit} style={{width:100,padding:10,backgroundColor:'black',alignSelf:'center',marginTop:40,borderRadius:5}}>
                 <Text style={{fontWeight:'bold',color:'white',textAlign:'center'}}>SUBMIT</Text>
             </TouchableOpacity>
+
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={isModalVisible}
+            >
+                <View style={{ backgroundColor: 'white', width: '100%', alignSelf: 'center', height: 800, borderRadius: 10 }}>
+                    <TouchableOpacity onPress={handleclose} style={{ marginLeft: horizontalScale(150) }}>
+                        <MaterialIcons style={style.icon1} name="keyboard-arrow-left" color={'black'} size={34} />
+                    </TouchableOpacity>
+                    <Image
+                        source={{ uri: authData.user.imageURL }}
+                        style={{ width: 400, height: 500, marginTop: 100, alignSelf: 'center' }}
+                    />
+                </View>
+            </Modal>
         </View>
     )
 }
@@ -141,5 +167,10 @@ const style = StyleSheet.create({
         backgroundColor: 'white',
         width: 40,
         textAlign: 'center'
-    }
+    },
+    icon1: {
+        marginTop: 15,
+        marginHorizontal: 16,
+        marginRight: 150,
+    },
 })
